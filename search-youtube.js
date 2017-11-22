0472ec04b410dc9258f452e460b93c92d5a1c91c
Margaret Blauvelt <margaret@blauvelt.com>
2017-11-21 23:11:52 -0800

    Explore returned JSON via console logging
    Verify which URL best for thumbnail, location of video title, video ID, etc.

--- search-youtube.js
+++ search-youtube.js
@@ -1,4 +1,5 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const YOUTUBE_WATCH_URL = 'https://www.youtube.com/watch?v='
 
 // Function: getDataYouTubeAPI
 // Get data from YouTube
@@ -11,9 +12,44 @@ function getDataYouTubeAPI(searchTerm, callback) {
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
-function displayYouTubeData() {
+function displayYouTubeData(data) {
   console.log("Made it to displayYouTubeData");
+  console.log(data.kind);  // youtube#searchListResponse
+  console.log(data.pageInfo);  // {totalResults: 1000000, resultsPerPage: 5}
+  console.log("Start data.items[0]");
+  console.log(data.items[0]);
+  console.log("End data.items[0]");
+  // console.log("Start data.items[0].kind");
+  // console.log(data.items[0].kind);
+  // console.log("End data.items[0].kind");
+  console.log("start data.items[0].id");
+  console.log(data.items[0].id);
+  console.log("end data.items[0].id");
+  console.log("start data.items[0].id.videoId");
+  console.log(data.items[0].id.videoId);
+  console.log("end data.items[0].id.videoId");
+  console.log("The image url follows");
+  console.log(data.items[0].snippet.thumbnails.high.url);
+  // const results = data.items.map((item, index) => renderResult(item));
+  const results = data.items.map(function(item) {
+    renderResult(item);
+  })
+  $('.js-results').html(results);
 }
+
+function renderResult(result) {
+  const keys = Object.keys(result);
+  const values = Object.values(result);
+  console.log(keys);
+  console.log(values);
+
+  console.log("Leaving renderResult");
+  return `
+    ${result.items}
+  `;
+}
+
+
 
 // Function: watchSubmit
 // Wait for user to submit desired search terms
